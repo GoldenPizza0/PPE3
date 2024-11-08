5d8a77f6e4992f8ae284f5e594f4d4b1d7c4fd85
Sofiane <chakrouns@saintemarie-cholet.eu>
2024-11-08 11:15:25 +0100

    mise en place de la partie connexion en construction

---
+++ controleur/connexion.php
@@ -0,0 +1,36 @@
+<?php
+if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
+    $racine="..";
+}
+include_once "$racine/modele/authentification.php";
+
+
+// recuperation des donnees GET, POST, et SESSION
+if (isset($_POST["username"]) && isset($_POST["mdp"])){
+    $mailU=$_POST["username"];
+    $mdpU=$_POST["username"];
+}
+else
+{
+    $mailU="";
+    $mdpU="";
+}
+
+// appel des fonctions permettant de recuperer les donnees utiles a l'affichage 
+
+
+// traitement si necessaire des donnees recuperees
+login($mailU,$mdpU);
+
+if (isLoggedOn()){ // si l'utilisateur est connecté on redirige vers le controleur monProfil
+    //include "$racine/controleur/monProfil.php";
+}
+else{ // l'utilisateur n'est pas connecté, on affiche le formulaire de connexion
+    // appel du script de vue 
+    $titre = "authentification";
+    include "$racine/vue/entete.php";
+    include "$racine/vue/vueAuthentification.php";
+    include "$racine/vue/pied.php";
+}
+
+?>
---
+++ modele/bd.utilisateur.inc.php
@@ -0,0 +1,58 @@
+<?php
+
+include_once "bd.inc.php";
+
+function getUtilisateurs() {
+
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from profil");
+        $req->execute();
+
+        $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        while ($ligne) {
+            $resultat[] = $ligne;
+            $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        }
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function getUtilisateurByMailU($username) {
+
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from profil where username=:username");
+        $req->bindValue(':username', $username, PDO::PARAM_STR);
+        $req->execute();
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function addUtilisateur($username, $mdp) {
+    try {
+        $cnx = connexionPDO();
+
+        $mdpUCrypt = crypt($mdp, "sel");
+        $req = $cnx->prepare("insert into utilisateur (mailU, mdpU) values(:mailU,:mdpU)");
+        $req->bindValue(':username', $username, PDO::PARAM_STR);
+        $req->bindValue(':mdp', $mdpUCrypt, PDO::PARAM_STR);
+        
+        $resultat = $req->execute();
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+
+?>
