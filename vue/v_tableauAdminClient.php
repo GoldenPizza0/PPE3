ebd733805eb48ab0a730fc62f3774a0f8f617893
dimitri <dimitriloiseau2@gmail.com>
2024-11-29 09:03:11 +0100

    création de la v4 de dimitri

--- vue/v_tableauAdminClient.php
+++ vue/v_tableauAdminClient.php
@@ -30,7 +30,9 @@ foreach($lesClientsEtSites as $UnClient){
                 <td><?php echo $UnClient["societe"];?></td>
                 <td><?php echo $UnClient["adresse"];?></td>
                 <td><?php echo $UnClient["activite"];?></td>
-                <td><a href="./?uc=client"><button name="button" value= <?php echo $UnClient['code_client'];?>>afficher</button></a></td>
+                <td><a href="./?uc=client"><button name="button" value= <?php echo $UnClient['code_client'];?>>afficher</button></a>
+                <a href="./?uc=client" img="./images/"><button name="button" value= <?php echo $UnClient['code_client'];?>>afficher</button></a>
+                <a href="./?uc=client"><button name="button" value= <?php echo $UnClient['code_client'];?>>afficher</button></a></td>
             </tr>
             <?php
             if ($_POST['button'] == $UnClient['code_client']){
