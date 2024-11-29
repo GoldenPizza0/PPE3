5b2f0fdf195456c72453d3436a428e9e094e1507
dimitri <dimitriloiseau2@gmail.com>
2024-11-29 10:19:08 +0100

    finission du tableau et correction de la fonction getlesclientsetsites

--- modele/bd.client.inc.php
+++ modele/bd.client.inc.php
@@ -25,7 +25,7 @@ function getLesClientsEtSites() {
             SELECT code_client,adresse,societe,libelle_act as id_act FROM client join secteur on client.id_act = secteur.id_act
         ");
         $req->execute();
-        $listeclient = $req->fetchAll(PDO::FETCH_ASSOC);
+        $listeclient = $req->fetchALL(PDO::FETCH_ASSOC);
         foreach($listeclient as $client){
             $cnx = connexionPDO();
             $req = $cnx->prepare("
@@ -33,8 +33,8 @@ function getLesClientsEtSites() {
                 where code_client = ".$client['code_client']." 
             ");
             $req->execute();
-            $listesite = $req->fetchAll(PDO::FETCH_ASSOC);
-            $listesites = [];
+            $listesite = $req->fetchALL(PDO::FETCH_ASSOC);
+            $listesites = array();
             foreach($listesite as $site){
                 $unsite = [
                     'num_site' => $site['num_site'],
@@ -42,7 +42,7 @@ function getLesClientsEtSites() {
                     'adresse_site' => $site['adresse_site'],
                     'referent' => $site['referent']
                 ];
-                $listesites = [$unsite];
+                array_push($listesites,$unsite);
             }
             $unclient = [
                 'code_client' => $client['code_client'],
@@ -52,7 +52,7 @@ function getLesClientsEtSites() {
                 'sites' => $listesites,
 
             ];
-            $lesclients = [$unclient];
+            array_push($lesclients,$unclient);
         }
         return $lesclients;
     } catch (Exception $e) {
