3586f1778f458c58e701cee8b4eb80ab044fadd1
Calliste Godailler <calliste.godailler@gmail.com>
2024-11-29 11:41:31 +0100

    demarrage de tout ce qui est lié aux interventions

---
+++ controleur/c_gererIntervention.php
@@ -0,0 +1,75 @@
+﻿<?php
+	if (isset($_GET["action"])) {
+		$action = $_GET["action"];
+	} 
+	else {
+		$action = "voirInterventions";
+	}
+
+	if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
+		$racine = "..";
+	}
+
+	include "vue/entete.php";
+	include_once "$racine/modele/bd.intervention.inc.php";
+
+	switch($action)
+	{
+		case 'voirInterventions':
+		{
+			$lesContrats = getLesContrats();
+			include("vue/v_contratInterventions.php");
+		break;
+		}
+
+		/*
+		case 'creationIntervenant':
+		{
+			include("vue/v_creationIntervenant.php");
+			break;
+		}
+		case 'confirmCreatIntervenant':
+		{
+			$nom = $_REQUEST['TNom'];
+			$prenom = $_REQUEST['TPrenom'];
+			$niveauEtudes = $_REQUEST['TNE'];
+			$maitreAnglais = $_REQUEST['TMA'];
+			$pdo->creerIntervenant($nom,$prenom,$niveauEtudes,$maitreAnglais);
+			
+			include("vue/v_intervenants_redirection.php");	
+			break;
+		}
+		
+		case 'modificationIntervenant':
+		{
+			include("vue/v_modificationIntervenant.php");
+			break;
+		}
+		case 'confirmModifIntervenant':
+		{
+			$id_Intervenant = $_REQUEST['TId'];
+			$nom = $_REQUEST['TNom'];
+			$prenom = $_REQUEST['TPrenom'];
+			$niveauEtudes = $_REQUEST['TNE'];
+			$maitreAnglais = $_REQUEST['TMA'];
+			$pdo->modifierIntervenant($id_Intervenant,$nom,$prenom,$niveauEtudes,$maitreAnglais);
+			
+			include("vue/v_intervenants_redirection.php");	
+			break;
+		}
+
+		case 'suppressionIntervenant':
+		{
+			include("vue/v_suppressionIntervenant.php");
+			break;
+		}
+		case 'confirmSuppIntervenant':
+		{
+			$id_Intervenant = $_REQUEST['TId'];
+			$pdo->supprimerIntervenant($id_Intervenant);
+			
+			include("vue/v_intervenants_redirection.php");	
+			break;
+		}*/
+	}
+?>
