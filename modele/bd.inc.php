df9e8c5345edd39767b58a3ed05be487b67ca2ab
Calliste Godailler <calliste.godailler@gmail.com>
2024-12-11 20:02:11 +0100

    com serveur decom localhost

--- modele/bd.inc.php
+++ modele/bd.inc.php
@@ -1,15 +1,15 @@
 <?php
 
 function connexionPDO() {
-    $login = "root";
-    $mdp = "";
-    $bd = "esn_steria";
-    $serveur = "localhost";
+    // $login = "root";
+    // $mdp = "";
+    // $bd = "esn_steria";
+    // $serveur = "localhost";
 
-    // $login = "dbo672809222";
-    // $mdp = "4FsiBA8FYNuk";
-    // $bd = "db672809222";
-    // $serveur = "db672809222.db.1and1.com";
+    $login = "dbo672809222";
+    $mdp = "4FsiBA8FYNuk";
+    $bd = "db672809222";
+    $serveur = "db672809222.db.1and1.com";
 
     try {
         $conn = new PDO("mysql:host=$serveur;dbname=$bd", $login, $mdp, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'')); 
