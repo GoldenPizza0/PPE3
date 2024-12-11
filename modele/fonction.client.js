ac896611081ea7d72676951720733fb6d96922e0
Calliste Godailler <calliste.godailler@gmail.com>
2024-12-11 19:29:35 +0100

    suppression fichiers superflus

--- modele/fonction.client.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    function MontrerSitesDeClients($id){
-        document.getElementByValue("sous-tableau").hidden = true;
-        document.getElementById($id).hidden = false;
-    };
-)};
