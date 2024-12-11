f27c3de33b78d82ab2582f5e8fc0ea0907ccc979
Calliste Godailler <calliste.godailler@gmail.com>
2024-12-11 19:24:05 +0100

    correction bourde dimitri

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 
-<link href="modele/cssGeneral.css" rel="stylesheet" type="text/css" /> 
+<link href="css/cssGeneral.css" rel="stylesheet" type="text/css" /> 
 <?php
 include "getRacine.php";
 include "$racine/controleur/controleurPrincipal.php";
 include_once "$racine/modele/authentification.php"; // pour pouvoir utiliser isLoggedOn()
+require_once("modele/class.pdoSteria.inc.php");
+$pdo = PdoSteria::getPdoEsteria();
 if (isset($_GET["uc"])) {
     $uc = $_GET["uc"];
 } 
