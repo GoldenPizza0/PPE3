9a3ddc7d79fe61cbd1d495d1ab43e5e0ddbd2ffd
dimitri <dimitriloiseau2@gmail.com>
2024-12-11 19:17:51 +0100

    modifier les images boutons de tableau admin client

--- vue/v_tableauAdminClient.php
+++ vue/v_tableauAdminClient.php
@@ -33,8 +33,8 @@ foreach($lesClientsEtSites as $UnClient){
                 <td><?php echo $UnClient["societe"];?></td>
                 <td><?php echo $UnClient["adresse"];?></td>
                 <td><?php echo $UnClient["activite"];?></td>
-                <td><a href="./?uc=client&action=SupprimerClient&id=<?php echo $UnClient['code_client'];?>" onclick="return confirm('voulez vous vraiment supprimer ce client')"><button>X</button></a></td>
-                <td><a href="./?uc=client&action=ModifierClient&id=<?php echo $UnClient['code_client'];?>"><button>%</button></a></td>
+                <td><a href="./?uc=client&action=SupprimerClient&id=<?php echo $UnClient['code_client'];?>" onclick="return confirm('voulez vous vraiment supprimer ce client')"><img src="images/supp.png" title="Suppr"></a></td>
+                <td><a href="./?uc=client&action=ModifierClient&id=<?php echo $UnClient['code_client'];?>"><img src="images/modifier.gif" title="Modif"></a></td>
                 <td><a href="./?uc=client&id=<?php echo $UnClient['code_client'];?>"><button name="idClient" value= <?php echo $UnClient['code_client'];?>>afficher</button></a></td>
                 <td><a href="./?uc=client&action=CreerSite&id=<?php echo $UnClient['code_client'];?>"><button>nouveau site</button></a></td>
             </tr>
@@ -55,8 +55,8 @@ foreach($lesClientsEtSites as $UnClient){
                             <td><?php echo $UnSite["nom_site"];?></td>
                             <td><?php echo $UnSite["adresse_site"];?></td>
                             <td><?php echo $UnSite["referent"];?></td>
-                            <td><a href="./?uc=client&action=SupprimerSite&id=<?php echo $UnClient['code_client'];?>&site=<?php echo $UnSite['num_site'];?>"><button>X</button></a></td>
-                            <td><a href="./?uc=client&action=ModifierSite&id=<?php echo $UnClient['code_client'];?>&site=<?php echo $UnSite['num_site'];?>"><button>%</button></a></td>
+                            <td><a href="./?uc=client&action=SupprimerSite&id=<?php echo $UnClient['code_client'];?>&site=<?php echo $UnSite['num_site'];?>"><img src="images/modifier.gif" title="Modif"></a></td>
+                            <td><a href="./?uc=client&action=ModifierSite&id=<?php echo $UnClient['code_client'];?>&site=<?php echo $UnSite['num_site'];?>"><img src="images/modifier.gif" title="Modif"></a></td>
                         </tr>
                     <?php
                     }
